d6224635980a3980382a8a33c4d0fd2c81dfe644
labato71 <103439684+labato71@users.noreply.github.com>
2026-03-01 03:29:21 +0300

    Create AutoTableLock.java

---
+++ src/main/java/me/haci/autolock/AutoTableLock.java
@@ -0,0 +1,11 @@
+package me.haci.autolock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutoTableLock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("AutoTableLock aktif.");
+    }
+}
